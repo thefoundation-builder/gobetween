c9c4b2c442619a3f0b309c55adadd8b4aa9d2cdd
Benjamin Freitag <gitlab.com@b1nch0.us.to>
2023-02-01 06:47:43 +0000

    Update 2 files
    - /Dockerfile
    - /install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+MYARCH="build"
+cd /usr/bin/ && uname -a |grep -e amd64 -e x86_86  && MYARCH=amd64 
+echo "get  release page"
+RELEASES=$(curl -s https://github.com/yyyar/gobetween/tags|grep /releases/tag |grep href|cut -d'"' -f4|grep /releases|head -n1)
+TARGFILE=$(curl -s $(curl -s https://github.com/$RELEASES|grep  expanded_assets|sed 's/.\+src="//g'|cut -d '"' -f1)|grep /releases/|cut -d'"' -f2|grep MYARCH)
+echo "getting $TARGFILE FROM $RELEASES"
+( curl -kLv https://github.com/$TARGFILE |tar xvz)
+
+#( curl -kLv https://github.com/$(curl -s $(curl -s https://github.com/$(curl -s https://github.com/yyyar/gobetween/tags|grep /releases/tag |grep href|cut -d'"' -f4|grep /releases|head -n1)|grep  expanded_assets|sed 's/.\+src="//g'|cut -d '"' -f1)|grep /releases/|cut -d'"' -f2|grep MYARCH) |tar xvz)
