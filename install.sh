0e341e1cbc65a9e853acad1dc05ee2e15eaedb54
Benjamin Freitag <gitlab.com@b1nch0.us.to>
2023-02-01 06:59:40 +0000

    Update file install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ uname -a |grep -e amd64 -e x86_64  && MYARCH=amd64
 echo "get  release page"
 RELEASES=$(curl -s https://github.com/yyyar/gobetween/tags|grep /releases/tag |grep href|cut -d'"' -f4|grep /releases|head -n1)
 RELEASEDOWNLOADS=$(curl -s https://github.com/$RELEASES|grep  expanded_assets|sed 's/.\+src="//g'|cut -d '"' -f1)
-TARGFILE=$(curl -s $RELEASEDOWNLOADS|grep /releases/|cut -d'"' -f2|grep $MYARCH)
+TARGFILE=$(curl -s $RELEASEDOWNLOADS|grep /releases/|cut -d'"' -f2|grep $MYARCH |grep linux |grep tar.gz |head -n1 )
 echo "getting for ARCH $MYARCH TARGET= $TARGFILE FROM $RELEASES == $RELEASEDOWNLOADS"
 [[ "$MYARCH" = "build" ]] || ( curl -kLv https://github.com/$TARGFILE |tar xvz)
 
-#( curl -kLv https://github.com/$(curl -s $(curl -s https://github.com/$(curl -s https://github.com/yyyar/gobetween/tags|grep /releases/tag |grep href|cut -d'"' -f4|grep /releases|head -n1)|grep  expanded_assets|sed 's/.\+src="//g'|cut -d '"' -f1)|grep /releases/|cut -d'"' -f2|grep $MYARCH)|grep linux |grep tar.gz |head -n1|tar xvz)
+#( curl -kLv https://github.com/$(curl -s $(curl -s https://github.com/$(curl -s https://github.com/yyyar/gobetween/tags|grep /releases/tag |grep href|cut -d'"' -f4|grep /releases|head -n1)|grep  expanded_assets|sed 's/.\+src="//g'|cut -d '"' -f1)|grep /releases/|cut -d'"' -f2|grep $MYARCH|grep linux |grep tar.gz |head -n1)|tar xvz)
