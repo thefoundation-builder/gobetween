8b19b29c3aa3dcf07a0036f872ca548fd2a1c3f9
ı øŧħ³® ¢øµµ→ŧŧ€® <you@example.com>
2023-02-01 14:18:21 +0100

    2023-02-01-13.18

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM alpine
 
-RUN apk update && apk upgrade && apk add curl bash 
+RUN apk update && apk upgrade && apk add curl bash socat
 COPY  install.sh /
 RUN bash /install.sh
 RUN chmod +x /usr/bin/gobetween
 RUN mkdir /conf
 RUN gobetween --help |grep balancer
-ENTRYPOINT /bin/bash -c 'timeout 7776000 /usr/bin/gobetween -c /conf/gobetween.conf'
 
+ENTRYPOINT /bin/bash -c 'timeout 7776000 /usr/bin/gobetween -c /conf/gobetween.conf'
+ENTRYPOINT /bin/bash /boot.sh
+COPY boot.sh /
 CMD  /usr/bin/gobetween
 
 #CMD /bin/ash
---
+++ boot.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "launching gobetween"
+[[ -z $BINDPORT ]] && BINDPORT=11111
+[[ -z $MINTLS ]] && MINTLS=1.2
+
+[[ -z "$PRIVKEY" ]] || [[ -z "$CERTPEM" ]] || test -e ${PRIVKEY} && test -e ${CERTPEM} && (
+
+echo "init:socat"
+cat ${PRIVKEY} ${CERTPEM} > /tmp/socat.pem
+socatcmd="socat OPENSSL-LISTEN:6636,reuseaddr,pf=ip4,fork,cert=/tmp/socat.pem,cafile=${CERTPEM}, verify=0,openssl-min-proto-version=TLS${MINTLS} tcp-connect:127.0.0.0:11111"
+echo "SOCAT COMMAND: $socatcmd"
+
+while (true); do
+bash -c "$socatcmd"
+sleep 0.5
+while (true); do
+oldsum=$(cat /tmp/socat.pem|md5sum)
+newsum=$(cat ${PRIVKEY} ${CERTPEM})
+
+[[ "$oldsum" ]] = [[ "$newsum" ]] || (
+   echo "CERTs CHANGED, restarting socat"
+   cat ${PRIVKEY} ${CERTPEM} > /tmp/socat.pem
+   kill -9 $(pidof socat)
+)
+sleep 180
+done
+) &
+exec /usr/bin/gobetween
